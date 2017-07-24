e007e4d1374c8e3e2d8eeb0e58fc6a07993e09f2
謝頴華 <procandi@gmail.com>
2017-07-24 13:04:05 +0800

    aaa
    aaa

--- Main.rb
+++ Main.rb
@@ -14,17 +14,21 @@ class Main < Sinatra::Base
   
   #開始直播
   get '/facebook_live' do
-  	p params[:stream_url]
+  	@stream_url=params[:stream_url]
 	erb :facebook_live
   end
 
 
   post '/begin_live' do
-  	p params[:OSName]
+  	OS=params[:OSName]
+  	stream_url=params[:stream_url]
+  	p OS
+  	p stream_url
+
 	if OS=='OS X'
-		#result=`ffmpeg -f avfoundation -i "Capture screen 0:Built-in Input" -f flv "#{stream_url}"`
+		result=`ffmpeg -f avfoundation -i "Capture screen 0:Built-in Input" -f flv "#{stream_url}"`
 	elsif OS=='UNIX' || OS=='LINUX'
-		#result=`ffmpeg -f x11grab -thread_queue_size 100 -r 10 -video_size 1024x768 -i :0 -f alsa -thread_queue_size 500 -ar 44100 -ac 1 -i default -f flv -c:v h264 -c:a aac "#{stream_url}"`
+		result=`ffmpeg -f x11grab -thread_queue_size 100 -r 10 -video_size 1024x768 -i :0 -f alsa -thread_queue_size 500 -ar 44100 -ac 1 -i default -f flv -c:v h264 -c:a aac "#{stream_url}"`
 	end
 
 
